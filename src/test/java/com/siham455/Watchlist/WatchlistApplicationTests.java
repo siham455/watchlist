5b51eae83d116831c9c6db8b0dc99b18f44a629e
siham455 <siham455@outlook.com>
2025-02-01 01:06:33 +0000

    first commit

---
+++ src/test/java/com/siham455/Watchlist/WatchlistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.siham455.Watchlist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WatchlistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
