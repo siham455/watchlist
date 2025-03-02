81f72f4a8e5208f759a8dac09213ab5be425aa7c
siham455 <siham455@outlook.com>
2025-03-02 14:43:15 +0000

    .

--- src/main/java/com/siham455/Watchlist/users/UserService.java
+++ src/main/java/com/siham455/Watchlist/users/UserService.java
@@ -10,49 +10,49 @@ import org.springframework.stereotype.Service;
 @Service
 public class UserService{
 
-private UserRepository userRepository;
+    private UserRepository userRepository;
 
-public UserService(UserRepository userRepository) {
-    this.userRepository = userRepository;
-}
-
-public List<User> getAllUsers() {
-    return this.userRepository.findAll();
-}
-
-public User getUserById(UUID id) throws NoSuchElementException {
-    return userRepository.findById(id).orElseThrow();
-}
-
-
-public User createUser(User user) throws IllegalArgumentException, OptimisticLockingFailureException {
-    
-    if (user.getFirstName() == null || user.getFirstName().isEmpty()) {
-        throw new IllegalArgumentException("First name cannot be empty");
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
     }
-    if (user.getLastName() == null || user.getLastName().isEmpty()) {
-        throw new IllegalArgumentException("Last name cannot be empty");
-    }
-    
-    this.userRepository.save(user);
-    return user;
-}
 
-// Update a user
-public User updateUser(UUID id, User updatedUser) throws NoSuchElementException {
-    User user = userRepository.findById(id).orElseThrow();
-    user.setFirstName(updatedUser.getFirstName());
-    user.setLastName(updatedUser.getLastName());
-    return userRepository.save(user);
-}
-
-// Delete a user
-public void deleteUser(UUID id) throws NoSuchElementException {
-    if (userRepository.existsById(id)) {
-        userRepository.deleteById(id);
-    } else {
-        throw new NoSuchElementException();
+    public List<User> getAllUsers() {
+        return this.userRepository.findAll();
     }
-}
+
+    public User getUserById(UUID id) throws NoSuchElementException {
+        return userRepository.findById(id).orElseThrow();
+    }
+
+
+    public User createUser(User user) throws IllegalArgumentException, OptimisticLockingFailureException {
+        
+        if (user.getFirstName() == null || user.getFirstName().isEmpty()) {
+            throw new IllegalArgumentException("First name cannot be empty");
+        }
+        if (user.getLastName() == null || user.getLastName().isEmpty()) {
+            throw new IllegalArgumentException("Last name cannot be empty");
+        }
+        
+        this.userRepository.save(user);
+        return user;
+    }
+
+    // Update a user
+    public User updateUser(UUID id, User updatedUser) throws NoSuchElementException {
+        User user = userRepository.findById(id).orElseThrow();
+        user.setFirstName(updatedUser.getFirstName());
+        user.setLastName(updatedUser.getLastName());
+        return userRepository.save(user);
+    }
+
+    // Delete a user
+    public void deleteUser(UUID id) throws NoSuchElementException {
+        if (userRepository.existsById(id)) {
+            userRepository.deleteById(id);
+        } else {
+            throw new NoSuchElementException();
+        }
+    }
 
 }
